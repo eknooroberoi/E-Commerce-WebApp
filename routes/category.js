d3acb7151d1085efb32aaa6415cc7c44f2451d0f
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-06-19 03:36:20 +0530

    creating category using postman

--- routes/category.js
+++ routes/category.js
@@ -8,8 +8,10 @@ const {
     create 
  } = require("../controllers/category");
 
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
-router.post("/category/create", create);
-
+router.post("/category/create/:userId", requireSignin, isAdmin, isAuth , create);
+router.param("userId", userById);
 
 module.exports= router;
