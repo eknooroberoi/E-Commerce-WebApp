cbe3e28ccd4cf67c9877b3bde2180c5901a126f4
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-06-20 00:13:21 +0530

    product delete

--- routes/product.js
+++ routes/product.js
@@ -4,12 +4,15 @@ const express = require("express");
 const router = express.Router();
 
 //create= to create new category
-const { create, productById, read } = require("../controllers/product");
+const { create, productById, read, remove } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+const { route } = require("./auth");
 
-router.get("/product/:productId", read)
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, isAuth , create);
+router.delete("/product/:productId/:userId", requireSignin, isAdmin, isAuth, remove);
+
 router.param("userId", userById);
 router.param("productId", productById);
 
