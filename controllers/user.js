ecdb3da0a9558cf750ccaf84bb77cb95e7951811
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-06-20 07:11:52 +0530

    read and update user profile

--- controllers/user.js
+++ controllers/user.js
@@ -13,3 +13,29 @@ exports.userById = (req, res, next, id) => {
         next();
     });
 };
+
+exports.read =(req,res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+
+//newly updated records are sent to front end as json response
+exports.update = (req, res) => {
+    User.findOneAndUpdate(
+        {_id: req.profile._id}, 
+        {$set: req.body}, 
+        {new: true}, 
+        (err, user) => {
+            if(err) {
+                return res.status(400).json({
+                    error: "You are not authorized to perform this action"
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        }
+        );
+};
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { userById } = require("../controllers/user");
+const { userById, read, update } = require("../controllers/user");
 //loggedin user and authenticated user must have same id ie. a user now can't enter someone elses id and access it
 //if we put isAdmin then only if role=1 ie admin can access it
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
@@ -11,6 +11,13 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
         user: req.profile
     });
 });
+
+
+//allow the user to see and update profile
+//see
+router.get("/user/:userId", requireSignin, isAuth, read);
+//update
+router.put("/user/:userId", requireSignin, isAuth, update);
 
 // we want to check parameter,anytime there is parameter userId in the route we want to execute userById method and make user info available in the request object
 router.param("userId", userById);
